d62fda41ffed96ac043d2abfd28b0d4d55a1047c
Daniel Yoo <Daniel Yoo>
2016-10-05 16:34:05 -0400

    initial commite

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function testing()
+    {
+        dd(Auth::user()->email);
+    }
+
+    public function register()
+    {
+        dd('register?');
+    }
+}
