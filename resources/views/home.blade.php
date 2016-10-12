5fd0c95d7146d89a771f0b294db24ca18752d4c1
Ramy Sandouk <rg605@live.com>
2016-10-12 13:29:32 -0400

    changed welcome view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">BLABLASBLASBLDABLASDB CONTENT</div>
 
                 <div class="panel-body">
-                    Welcome!
+                    Welcome here!
                 </div>
             </div>
         </div>
