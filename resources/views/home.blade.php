c660456f2d2350c43445618252050b7ce4703d3a
woozzie <chriskfwoo@gmail.com>
2016-11-19 19:44:28 -0500

    working on view page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,172 +1,35 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset='utf-8' />
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.header')
 
-<!-- CSRF Token -->
-<meta name="csrf-token" content="{{ csrf_token() }}">
+@section('content')
+<div class="container-full">
+    <div class="row">
 
-<title>{{ config('app.name', 'Concordinate') }}</title>
+        <div id="sidemenu" class="col-sm-2">
+          <p style="text-align: center">Choose A Program Schedule</p>
+          <div class="program-schedule">
+            <!-- make sure the size is variable -->
+             <select name="programschedule" size="3">
+                 <option value="1" onclick="myFunction(this.value)">Program Schedule 1</option>
+                  <option value="2" onclick="myFunction(this.value)">Program Schedule 2</option>
+                   <option value="3" onclick="myFunction(this.value)">Program Schedule 3</option>
 
-<!-- Styles -->
-<link href="/css/app.css" rel="stylesheet">
-<link href="/css/custom.css" rel="stylesheet">
-<link href='css/fullcalendar.css' rel='stylesheet' />
-<link href='css/fullcalendar.print.css' rel='stylesheet' media='print' />
-<script src='js/moment.min.js'></script>
-<script src='js/jquery.min.js'></script>
-<script src='js/fullcalendar.min.js'></script>
+             </select>
 
-
-<!-- Custom CSS -->
-<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
-
-
-<!-- Scripts -->
-<script>
-    window.Laravel = <?php echo json_encode([
-        'csrfToken' => csrf_token(),
-    ]); ?>
-</script>
-
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
-
-
-
-<script>
-
-	$(document).ready(function() {
-
-		$('#calendar').fullCalendar({
-			header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month,agendaWeek,agendaDay,listWeek'
-			},
-			defaultDate: '2016-09-12',
-			navLinks: true, // can click day/week names to navigate views
-			editable: true,
-			eventLimit: true, // allow "more" link when too many events
-			events: [
-				{
-					title: 'All Day Event',
-					start: '2016-09-01'
-				},
-				{
-					title: 'Long Event',
-					start: '2016-09-07',
-					end: '2016-09-10'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2016-09-09T16:00:00'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2016-09-16T16:00:00'
-				},
-				{
-					title: 'Conference',
-					start: '2016-09-11',
-					end: '2016-09-13'
-				},
-				{
-					title: 'Meeting',
-					start: '2016-09-12T10:30:00',
-					end: '2016-09-12T12:30:00'
-				},
-				{
-					title: 'Lunch',
-					start: '2016-09-12T12:00:00'
-				},
-				{
-					title: 'Meeting',
-					start: '2016-09-12T14:30:00'
-				},
-				{
-					title: 'Happy Hour',
-					start: '2016-09-12T17:30:00'
-				},
-				{
-					title: 'Dinner',
-					start: '2016-09-12T20:00:00'
-				},
-				{
-					title: 'Birthday Party',
-					start: '2016-09-13T07:00:00'
-				},
-				{
-					title: 'Click for Google',
-					url: 'http://google.com/',
-					start: '2016-09-28'
-				}
-			]
-		});
-
-	});
-
-</script>
-<style>
-
-	body {
-		margin: 40px 10px;
-		padding: 0;
-		font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
-		font-size: 14px;
-	}
-
-	#calendar {
-		max-width: 400px;
-    max-height: 400px;
-		margin: 0 auto;
-	}
-
-</style>
-</head>
-<body>
-
-  <nav class="navbar navbar-full navbar-light bg-faded">
-    <div class="container">
-    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
-    <div class="collapse navbar-toggleable-md navbar-custom" id="navbarResponsive">
-      <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-      <ul class="nav navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ url('/scheduler') }}">Scheduler</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ url('/schedule') }}">View</a>
-        </li>
-        <li class="nav-item dropdown float-xs-right">
-          <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Account
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="{{ url('/completed') }}">Set Completed Courses</a>
-            <a class="dropdown-item" href="resetpassword.html">Change Password</a>
-            <a class="dropdown-item" href="{{ url('/logout') }}"
-              onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">Logout</a>
-            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                      {{ csrf_field() }}
-            </form>
+             <script>
+            //  $('select option:even').css({'background-color': 'white', color: "#400000"});
+            function myFunction(value){
+              document.getElementById("schedule-content").innerHTML = value;
+            }
+             </script>
           </div>
-        </li>
+        </div>
 
-      </ul>
+        <div id="schedule-content" class="col-sm-10">
+
+        </div>
+
+        </div>
+
     </div>
-  </div>
-  </nav>
-
-	<div id='calendar'></div>
-
-</body>
-</html>
+</div>
+@endsection
